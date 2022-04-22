07fbb970d65682bd5e1d9ef53b6c4598d3556d98
akash-007-kotnala <ghostmankotnala29@gmail.com>
2022-04-22 17:41:18 +0530

    commit merged

--- src/App.js
+++ src/App.js
@@ -10,18 +10,13 @@ import Assignment from "./components/student/Assignment/Assignment";
 
 import Verify from "./components/verify/Verify";
 
-<<<<<<< HEAD
-import Classrooms from "./components/student/Classrooms";
-import AssignmentStream from "./components/student/AssignmentStream";
 import Discussion from "./components/Discussion/Discussion";
 import QuestionPage from "./components/Discussion/QuestionPage";
-=======
 import Classrooms from "./components/student/classrooms/Classrooms";
 import AssignmentStream from "./components/student/AssignmentStream/AssignmentStream";
 import Login from "./components/auth/Login";
 import SignUp from "./components/auth/SignUp";
 import ClassroomCreation from "./components/student/classrooms/ClassroomCreation";
->>>>>>> 6c7ee0cb1a60a926a2f1294a88542dfd10e2fbdb
 
 function App() {
   return (
@@ -32,8 +27,7 @@ function App() {
           <Route path="/faculty" element={<Home />} />
           <Route path="/upload" element={<UploadFile />} />
           <Route path="/verify/:userId" element={<Verify />} />
-<<<<<<< HEAD
-          <Route path="/" element={<Home />} />
+
           <Route
             path="/classroom/:classRoomId/discussion"
             element={<Discussion />}
@@ -42,10 +36,8 @@ function App() {
             path="/classroom/:classRoomId/discussion/question"
             element={<QuestionPage />}
           />
-=======
           <Route path="/signup" element={<SignUp />} />
           <Route path="/" element={<Login />} />
->>>>>>> 6c7ee0cb1a60a926a2f1294a88542dfd10e2fbdb
         </Routes>
       </BrowserRouter>
     </GlobalProvider>
