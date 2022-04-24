677a55dcbef3d23353f8fa343233ba724603fda6
akash-007-kotnala <ghostmankotnala29@gmail.com>
2022-04-25 01:08:21 +0530

    Dicussion Portal Finished

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -6,7 +6,7 @@ function ProtectedRoute() {
   let location = useLocation();
 
   var { token } = useContext(GlobalContext);
-  // console.log(token)
+
   if (token != null) return <Outlet />;
   return <Navigate to="/" state={{ from: location }} />;
 }
